3ed87b2cb2992f2eb78b87e18249850e63170ecd
hugriveros <hugoriveros14@gmail.com>
2025-11-27 17:29:00 -0300

    Configurar adaptador de Vercel para deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
+	output: 'static',
+	adapter: vercel({
+		webAnalytics: { enabled: true }
+	}),
 });
